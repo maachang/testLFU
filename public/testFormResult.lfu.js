7f6d0680b2d9d3718c0e81194ec37658d74f226d
maachang <maachang@gmail.com>
2022-10-20 21:07:13 +0900

    formテスト.

---
+++ public/testFormResult.lfu.js
@@ -0,0 +1,9 @@
+(function() {
+'use strict'
+
+exports.handler = function(resStatus, resHeader, request) {
+    console.log(JSON.stringify(request, null, "  "));
+    return {postParams: request.params};
+}
+
+})();
