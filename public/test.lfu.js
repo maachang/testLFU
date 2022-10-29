57d8aec5d5337a839a05896d2eeef9cab5b91ae8
maachang <maachang@gmail.com>
2022-10-29 21:34:32 +0900

    lfuSimurator対応.

--- public/test.lfu.js
+++ public/test.lfu.js
@@ -1,11 +1,7 @@
 (function() {
 'use strict'
 
-const httpsClient = frequire("./lib/httpsClient.js");
-
 exports.handler = function(resStatus, resHeader, request) {
-    console.log("## require: " + httpsClient);
-    console.log("## vm: " + frequire("vm"));
     return {hello: "world"};
 }
 
